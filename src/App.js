e679b6dbddb0404e671d839219b00f7e31fc29d2
Erwan31 <spilmonterwan@gmail.com>
2020-08-06 13:59:08 +0100

    Params + Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Profile from './Components/profile';
+import Home from './Components/home';
+import Post from './Components/post';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import PostItem from './Components/postitem';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <div className="d-flex flex-column flex-md-row align-item-center p-3 px-md-4 mb-3 border-bottom">
+          <h5 className="my-0 mr-md-auto font-weight-normal">MyApp</h5>
+          <nav className="my-2 my-md-0 mr-md-3">
+            <Link to="/" className="p-2 text-dark">Home</Link>
+            <Link to="/posts" className="p-2 text-dark">Posts</Link>
+            <Link to="/profile" className="p-2 text-dark">Profile</Link>
+          </nav>
+        </div>
       </header>
-    </div>
+      <Route path="/" component={Home} exact/>
+      <Route path="/posts" component={Post} exact/>
+      <Route path="/posts/:id" component={PostItem} exact/>
+      <Route path="/profile" component={Profile} exact/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostItem from './postitem';
+
+const Post = () => {
+    return ( 
+        <>
+            Posts
+        </>
+     );
+}
+ 
+export default Post;

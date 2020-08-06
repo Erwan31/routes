3b7b5c0b4e066be55e9070eecffc6859685c1654
Erwan31 <spilmonterwan@gmail.com>
2020-08-06 14:24:22 +0100

    Switch

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Profile from './Components/profile';
 import Home from './Components/home';
 import Post from './Components/post';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, NavLink, Redirect } from 'react-router-dom'
 import PostItem from './Components/postitem';
+import NotFound from './Components/404';
 
 
 function App() {
@@ -13,16 +14,33 @@ function App() {
         <div className="d-flex flex-column flex-md-row align-item-center p-3 px-md-4 mb-3 border-bottom">
           <h5 className="my-0 mr-md-auto font-weight-normal">MyApp</h5>
           <nav className="my-2 my-md-0 mr-md-3">
-            <Link to="/" className="p-2 text-dark">Home</Link>
-            <Link to="/posts" className="p-2 text-dark">Posts</Link>
-            <Link to="/profile" className="p-2 text-dark">Profile</Link>
+            <NavLink 
+            to="/" className="p-2 text-dark"
+            activeStyle={{fontWeight:'bold'}}
+            activeClassName="selected"
+            exact>Home</NavLink>
+            <NavLink 
+            to="/posts" className="p-2 text-dark"
+            activeStyle={{fontWeight:'bold'}}
+            activeClassName="selected">Posts</NavLink>
+            <NavLink 
+            to="/profile" className="p-2 text-dark"
+            activeStyle={{fontWeight:'bold'}}
+            activeClassName="selected">Profile</NavLink>
           </nav>
         </div>
       </header>
-      <Route path="/" component={Home} exact/>
-      <Route path="/posts" component={Post} exact/>
-      <Route path="/posts/:id" component={PostItem} exact/>
-      <Route path="/profile" component={Profile} exact/>
+      <div className="container">
+        <Switch>
+          <Redirect from="/profile" to="/"></Redirect>
+          <Route path="/posts/:id" component={PostItem}/>
+          <Route path="/posts" component={Post}/>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/" component={Home} exact/>
+          <Route component={NotFound}/>
+        </Switch>
+      </div>
+      
     </BrowserRouter>
   );
 }
---
+++ src/Components/404.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const NotFound = () => {
+    return ( 
+        <>
+            Not found
+        </>
+     );
+}
+ 
+export default NotFound;
